ad477b731867353505ac48fe33044bb741d4ba02
Kai Reinhard <K.Reinhard@micromata.de>
2018-08-24 07:59:44 +0200

    Initial revision.

---
+++ merlin-core/src/test/java/de/reinhard/merlin/Definitions.java
@@ -0,0 +1,18 @@
+package de.reinhard.merlin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class Definitions {
+    public static final File EXAMPLES_TEST_DIR = new File("examples/tests");
+
+    public static final File OUTPUT_DIR = new File("out");
+
+    static {
+        if (!OUTPUT_DIR.mkdirs()) {
+            throw new RuntimeException("Can't create test output directory '" + OUTPUT_DIR.getAbsolutePath() + "'");
+        }
+    }
+}
