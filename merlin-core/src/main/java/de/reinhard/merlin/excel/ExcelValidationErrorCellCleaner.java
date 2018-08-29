2cf1c46a37c2da792d746986a1c79f7be5007604
Kai Reinhard <K.Reinhard@micromata.de>
2018-08-29 23:16:05 +0200

    Panta rhei.

--- merlin-core/src/main/java/de/reinhard/merlin/excel/ExcelValidationErrorCellCleaner.java
+++ merlin-core/src/main/java/de/reinhard/merlin/excel/ExcelValidationErrorCellCleaner.java
@@ -2,7 +2,6 @@ package de.reinhard.merlin.excel;
 
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Row;
 
@@ -10,6 +9,8 @@ import java.util.Iterator;
 
 /**
  * Cleans an Excel sheet: gets rid of validation error messages, cell highlighted error cells and comments.
+ * This is use-full for downloading an Excel file containing validation errors, fixing it in the same Excel file and
+ * uploading it afterwards.
  */
 public class ExcelValidationErrorCellCleaner {
     public int clean(ExcelSheet sheet, ExcelWriterContext context) {
