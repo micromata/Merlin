1551c93bf511d69590d605915e5dac0b93b8845f
Kai Reinhard <K.Reinhard@micromata.de>
2018-08-29 15:32:55 +0200

    Initial revision.

---
+++ merlin-core/src/main/java/de/reinhard/merlin/excel/ExcelValidationErrorCellCleaner.java
@@ -0,0 +1,47 @@
+package de.reinhard.merlin.excel;
+
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.util.Iterator;
+
+/**
+ * Cleans an Excel sheet: gets rid of validation error messages, cell highlighted error cells and comments.
+ */
+public class ExcelValidationErrorCellCleaner {
+    public int clean(ExcelSheet sheet, ExcelWriterContext context) {
+        int columnWithErrorMessages = -1;
+        Row row = sheet.getHeadRow();
+        if (row != null) {
+            for (Cell cell : row) {
+                cell.getCellStyle().setFillPattern(FillPatternType.NO_FILL);
+            }
+            Cell lastCell = row.getCell(row.getLastCellNum() - 1);
+            if (PoiHelper.getValueAsString(lastCell).startsWith("***")) {
+                columnWithErrorMessages = lastCell.getColumnIndex();
+                row.removeCell(lastCell);
+                row.setHeight((short)-1);
+            }
+        }
+        Iterator<Row> it = sheet.getDataRowIterator();
+        while (it.hasNext()) {
+            row = it.next();
+            for (Cell cell : row) {
+                cell.getCellStyle().setFillPattern(FillPatternType.NO_FILL);
+                cell.removeCellComment();
+            }
+            // Remove cell with error messages.
+            if (columnWithErrorMessages >= 0) {
+                Cell cell = row.getCell(columnWithErrorMessages);
+                if (cell != null) {
+                    row.removeCell(cell);
+                    row.setHeight((short)-1);
+                }
+            }
+        }
+        return columnWithErrorMessages;
+    }
+}
