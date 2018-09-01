f432799baa33fb955a56fb71590eb7b805c93209
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-02 00:47:17 +0200

    Panta rhei.

--- merlin-core/src/test/java/de/reinhard/merlin/word/RunsParserTest.java
+++ merlin-core/src/test/java/de/reinhard/merlin/word/RunsParserTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
@@ -49,26 +50,43 @@ public class RunsParserTest {
     }
 
     @Test
+    public void parseStringList() {
+        assertArrayEquals(new String[0], RunsProcessor.parseStringList(null));
+        assertArrayEquals(new String[0], RunsProcessor.parseStringList(""));
+        assertArrayEquals(new String[]{"Berta"}, RunsProcessor.parseStringList("Berta"));
+        assertArrayEquals(new String[]{"Berta"}, RunsProcessor.parseStringList("„Berta“"));
+        assertArrayEquals(new String[]{"Berta", "Horst"}, RunsProcessor.parseStringList("„Berta“, \"Horst\""));
+        assertArrayEquals(new String[]{"Berta", "Horst"}, RunsProcessor.parseStringList("Berta, Horst"));
+        assertArrayEquals(new String[]{"Berta's", "Horst"}, RunsProcessor.parseStringList("„Berta's“, „Horst"));
+        assertArrayEquals(new String[]{"A", "B", "", "D"}, RunsProcessor.parseStringList("„A“, „B\", '', D"));
+        assertArrayEquals(new String[]{"A", "B", "", "D"}, RunsProcessor.parseStringList("„A“, „B\"  '' D"));
+        assertArrayEquals(new String[]{"A', „B", "", "D"}, RunsProcessor.parseStringList("„A', „B\"  '' D"));
+    }
+
+    @Test
     public void regexpTest() {
-        assertMatcher("{if Arbeitszeit = „Teilzeit“}", "Arbeitszeit", "=", "Teilzeit" );
+        assertMatcher("{if Arbeitszeit = „Teilzeit“}", "Arbeitszeit", "=", "Teilzeit");
         assertMatcher("{if Arbeitszeit = ‚Teilzeit‘}", "Arbeitszeit", "=", "Teilzeit");
         assertMatcher("{if Arbeitszeit != ‚Vollzeit‘}", "Arbeitszeit", "!=", "Vollzeit");
         assertMatcher("{if Arbeitszeit in ‚Vollzeit‘}", "Arbeitszeit", "in", "Vollzeit");
         assertMatcher("{if Arbeitszeit !in ‚Vollzeit‘}", "Arbeitszeit", "!in", "Vollzeit");
-        assertMatcher("{if Arbeitszeit !in ‚Vollzeit\"}");
+        assertMatcher("{if Arbeitszeit !in ‚Vollzeit\"}", "Arbeitszeit", "!in", "Vollzeit\"");
         assertMatcher("{if name = „Horst's“}", "name", "=", "Horst's");
     }
 
     private void assertMatcher(String str, String... groups) {
         Matcher matcher = RunsProcessor.beginIfPattern.matcher(str);
         assertEquals(groups.length > 0 ? true : false, matcher.find());
-        if (groups.length == 0 ) {
+        if (groups.length == 0) {
             return;
         }
-        assertEquals(groups.length, matcher.groupCount(), "Number of regexp group count.");
-        for (int i = 0; i < groups.length; i++) {
-            assertEquals(groups[i], RunsProcessor.removeQuotations(matcher.group(i + 1)));
+        assertEquals(3, matcher.groupCount(), "Number of regexp group count.");
+        assertEquals(groups[0], matcher.group(1));
+        assertEquals(groups[1], matcher.group(2));
+        String[] params = RunsProcessor.parseStringList(matcher.group(3));
+        assertEquals(groups.length - 2, params.length, "Number of comma separated values.");
+        for (int i = 2; i < groups.length; i++) {
+            assertEquals(groups[i], params[i - 2]);
         }
-
     }
 }
