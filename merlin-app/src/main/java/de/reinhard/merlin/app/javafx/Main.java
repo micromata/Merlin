35d7d74a01c5b0fdcbc2f4868e3343a13ad58eb9
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-05 01:29:47 +0200

    Panta rhei,.

--- merlin-app/src/main/java/de/reinhard/merlin/app/javafx/Main.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/javafx/Main.java
@@ -1,21 +1,34 @@
 package de.reinhard.merlin.app.javafx;
 
+import de.reinhard.merlin.app.jetty.JettyServer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private Logger log = LoggerFactory.getLogger(Main.class);
+    private JettyServer server;
+    private static Main main;
+
     public static void main(String[] args) {
         launch(args);
     }
 
+    static Main getInstance() {
+        return main;
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+        log.info("Starting Java FX application.");
+        main = this;
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("/Main.fxml"));
         ClassLoader cl = this.getClass().getClassLoader();
@@ -27,5 +40,18 @@ public class Main extends Application {
         stage.setTitle("Merlin");
         stage.setResizable(false);
         stage.show();
+        server = new JettyServer();
+        server.start();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        log.info("Stopping Java FX application.");
+        server.stop();
+    }
+
+    void openBrowser() {
+        getHostServices().showDocument("http://localhost:" + server.getPort() + "/");
     }
 }
