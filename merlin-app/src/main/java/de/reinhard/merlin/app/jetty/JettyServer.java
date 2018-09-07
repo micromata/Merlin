102382efbf53d35ffe12ddafc1de95bb83e24a53
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-07 16:19:31 +0200

    Pantha rhei.

--- merlin-app/src/main/java/de/reinhard/merlin/app/jetty/JettyServer.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/jetty/JettyServer.java
@@ -4,10 +4,13 @@ import de.reinhard.merlin.app.Configuration;
 import de.reinhard.merlin.app.rest.RestRegistry;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,26 +30,45 @@ public class JettyServer {
         }
         log.info("Starting web server on port " + port);
         server = new Server(port);
-
+/*
+        ServerConnector connector = new ServerConnector(server);
+        connector.setHost("127.0.0.1");
+        connector.setPort(port);
+        server.setConnectors(new Connector[]{connector});
+*/
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setDirectoriesListed(false);
-        resourceHandler.setWelcomeFiles(new String[]{ "resources/html/configure.html" });
-        resourceHandler.setResourceBase(".");
-
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
+        resourceHandler.setWelcomeFiles(new String[]{"index.html"});
         resourceHandler.setRedirectWelcome(false);
-        resourceHandler.setResourceBase("./merlin-webapp/build");
 
-        ServletHolder jerseyServlet = context.addServlet(ServletContainer.class, "/rest/*");
+        // Wrap the ResourceHandler in a ContextHandler for the "/" path
+        Resource basePath;
+        try {
+            basePath = Resource.newResource("./merlin-webapp/build");
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            return;
+        }
+        ContextHandler rootContextHandler = new ContextHandler();
+        rootContextHandler.setContextPath("/");
+        rootContextHandler.setBaseResource(basePath);
+        rootContextHandler.setHandler(resourceHandler);
+
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+
+        ErrorPageErrorHandler errorHandler = new ErrorPageErrorHandler();
+        errorHandler.addErrorPage(404, "/index.html");
+        contextHandler.setErrorHandler(errorHandler);
+
+        ServletHolder jerseyServlet = contextHandler.addServlet(ServletContainer.class, "/rest/*");
         jerseyServlet.setInitOrder(1);
         log.info("Rest services: " + RestRegistry.getInstance().getServiceList());
-        // Tells the Jersey Servlet which REST service/class to load.
+        // Tells the Jersey Servlxet which REST service/class to load.
         jerseyServlet.setInitParameter("jersey.config.server.provider.packages",
                 "de.reinhard.merlin.app.rest");
 
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] { resourceHandler, context });
+        handlers.setHandlers(new Handler[]{rootContextHandler, contextHandler});
         server.setHandler(handlers);
 
         try {
