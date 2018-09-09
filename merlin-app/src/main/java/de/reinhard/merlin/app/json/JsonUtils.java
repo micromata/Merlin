e4ca3ad7e732ebc224f55407f0e54f2e8a263a7e
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-09 10:22:51 +0200

    Unused methods removed.

--- merlin-app/src/main/java/de/reinhard/merlin/app/json/JsonUtils.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/json/JsonUtils.java
@@ -1,15 +1,12 @@
 package de.reinhard.merlin.app.json;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.List;
-import java.util.Map;
 
 public class JsonUtils {
     private static Logger log = LoggerFactory.getLogger(JsonUtils.class);
@@ -30,28 +27,6 @@ public class JsonUtils {
         ObjectMapper objectMapper = getObjectMapper();
         try {
             return objectMapper.readValue(json, clazz);
-        } catch (IOException ex) {
-            log.error(ex.getMessage(), ex);
-            return null;
-        }
-    }
-
-    public static <T> List<T> fromJsonToList(String jsonArray) {
-        ObjectMapper objectMapper = getObjectMapper();
-        try {
-            return objectMapper.readValue(jsonArray, new TypeReference<List<T>>() {
-            });
-        } catch (IOException ex) {
-            log.error(ex.getMessage(), ex);
-            return null;
-        }
-    }
-
-    public static <T> Map<String, T> fromJsonToMap(String jsonMap) {
-        ObjectMapper objectMapper = getObjectMapper();
-        try {
-            return objectMapper.readValue(jsonMap, new TypeReference<Map<String, T>>() {
-            });
         } catch (IOException ex) {
             log.error(ex.getMessage(), ex);
             return null;
