cb123c1293c8d234ca883fa98fc78a79c765d26f
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-11 21:54:56 +0200

    OS-Type not needed anymore.

--- merlin-app/src/main/java/de/reinhard/merlin/app/javafx/RunningMode.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/javafx/RunningMode.java
@@ -3,18 +3,8 @@ package de.reinhard.merlin.app.javafx;
 public class RunningMode {
     public enum Mode {TemplatesTest}
 
-    public enum OperatingSystem {MacOS, Windows, Other}
-
     public static Mode getMode() {
         return Mode.TemplatesTest;
-    }
-
-    public static OperatingSystem getOperationSystem() {
-        return OperatingSystem.MacOS;
-    }
-
-    public static boolean isMacOS() {
-        return getOperationSystem() == OperatingSystem.MacOS;
     }
 
     public static boolean isDevelopmentMode() {
