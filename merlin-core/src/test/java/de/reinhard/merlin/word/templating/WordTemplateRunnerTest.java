bc5054a81108f8f9af8113d5ebd942059e80819d
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-12 23:40:40 +0200

    Panta rhei.

--- merlin-core/src/test/java/de/reinhard/merlin/word/templating/WordTemplateRunnerTest.java
+++ merlin-core/src/test/java/de/reinhard/merlin/word/templating/WordTemplateRunnerTest.java
@@ -1,6 +1,7 @@
 package de.reinhard.merlin.word.templating;
 
 import de.reinhard.merlin.Definitions;
+import de.reinhard.merlin.excel.ExcelWorkbook;
 import de.reinhard.merlin.word.WordDocument;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ import java.util.Map;
 public class WordTemplateRunnerTest {
     private Logger log = LoggerFactory.getLogger(WordTemplateRunnerTest.class);
 
+    private final static String MY_WORKING_DIR = "/Users/kai/workspace/Micromata/MicromataV3/legalaffairs/Templates";
+
     @Test
     public void readWordTest() throws Exception {
         TemplateDefinition templateDefinition = DefinitionExcelConverterTest.create();
@@ -24,6 +27,43 @@ public class WordTemplateRunnerTest {
                 "4/1/2001", "25", "30");
         runDocument(runner, "berta", "Berta Charlson", "female", "8/14/2017",
                 "19/1/2017", "40", "30");
+        runMyFiles();
+    }
+
+    private void runMyFiles() throws Exception {
+        File dir = new File(MY_WORKING_DIR);
+        if (!dir.exists()) {
+            return;
+        }
+        log.info("Processing my-working-dir: " + MY_WORKING_DIR);
+        WordDocument templateDocument = new WordDocument(new File(MY_WORKING_DIR, "Arbeitsvertrag-Test-Template.docx"));
+        ExcelWorkbook workbook = new ExcelWorkbook(new File(MY_WORKING_DIR, "Arbeitsvertrag-Test-Template.xlsx"));
+        TemplateDefinitionExcelReader reader = new TemplateDefinitionExcelReader();
+        TemplateDefinition templateDefinition = reader.readFromWorkbook(workbook);
+        WordTemplateRunner runner = new WordTemplateRunner(templateDefinition, templateDocument);
+        Map<String, String> variables = new HashMap<>();
+        variables.put("Mitarbeiter", "Markus Meier");
+        variables.put("Geschlecht", "männlich");
+        variables.put("MA_Strasse", "ABC-Straße 5");
+        variables.put("MA_PLZ", "12345");
+        variables.put("MA_Ort", "Kassel");
+        variables.put("Vertragstyp", "unbefristet");
+        variables.put("Vertragsbeginn", "01.04.2017");
+        variables.put("Vertragsende", "---");
+        variables.put("Wochenstunden", "40");
+        variables.put("Urlaubstage", "30");
+        variables.put("Position", "Legalmitarbeiter");
+        variables.put("Aufgaben", "- Rechtliche Fragen rund um lorem epsum\n- Rechtliche Belange im Allgemeinen\n- Rechtliches im Speziellen.");
+        File file = new File(Definitions.OUTPUT_DIR, "Arbeitsvertrag-Markus.docx");
+        runDocument(runner, file, variables);
+        file = new File(Definitions.OUTPUT_DIR, "Arbeitsvertrag-Sandra.docx");
+        variables.put("Mitarbeiter", "Sandra Schmidt");
+        variables.put("Geschlecht", "weiblich");
+        variables.put("Wochenstunden", "20");
+        variables.put("Vertragstyp", "befristet");
+        variables.put("Vertragsende", "31.08.2017");
+        variables.put("Position", "Legalmitarbeiterin");
+        runDocument(runner, file, variables);
     }
 
     private void runDocument(WordTemplateRunner runner, String filenamepart, String employee, String gender, String date,
@@ -31,13 +71,19 @@ public class WordTemplateRunnerTest {
         Map<String, String> variables = new HashMap<>();
         variables.put("Employee", employee);
         variables.put("Gender", gender);
+        variables.put("Vertragstyp", "befristet");
+        variables.put("Vertragsende", "31.08.2017");
         variables.put("Date", date);
         variables.put("BeginDate", beginDate);
         variables.put("WeeklyHours", weeklyHours);
         variables.put("NumberOfLeaveDays", numberOfLeaveDays);
+        File file = new File(Definitions.OUTPUT_DIR, "Contract-" + filenamepart + ".docx");
+        runDocument(runner, file, variables);
+    }
+
+    private void runDocument(WordTemplateRunner runner, File file, Map<String, String> variables) throws Exception {
         WordDocument document = runner.run(variables);
         XWPFDocument doc = document.getDocument();
-        File file = new File(Definitions.OUTPUT_DIR, "Contract-" + filenamepart + ".docx");
         log.info("Writing modified MS Word file: " + file.getAbsolutePath());
         doc.write(new FileOutputStream(file));
     }
