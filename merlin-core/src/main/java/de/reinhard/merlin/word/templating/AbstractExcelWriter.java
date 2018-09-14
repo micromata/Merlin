dafe472cb3d57a6cd01d7df400a2926098019d5c
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-14 07:33:29 +0200

    AbstractExcelWriter extracted.

---
+++ merlin-core/src/main/java/de/reinhard/merlin/word/templating/AbstractExcelWriter.java
@@ -0,0 +1,114 @@
+package de.reinhard.merlin.word.templating;
+
+import de.reinhard.merlin.I18n;
+import de.reinhard.merlin.excel.ExcelCell;
+import de.reinhard.merlin.excel.ExcelRow;
+import de.reinhard.merlin.excel.ExcelSheet;
+import de.reinhard.merlin.excel.ExcelWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractExcelWriter {
+    private Logger log = LoggerFactory.getLogger(AbstractExcelWriter.class);
+
+    protected static final int COLUMN_WIDE_LENGTH = 5000;
+    protected static final int COLUMN_EXTRA_WIDE_LENGTH = 15000;
+
+    protected ExcelWorkbook workbook;
+    protected CellStyle titleStyle;
+    protected CellStyle headRowStyle;
+    protected CellStyle warningCellStyle;
+    protected CellStyle descriptionStyle;
+    protected ExcelSheet sheet;
+
+    protected void init() {
+        Workbook poiWorkbook = new XSSFWorkbook();
+        workbook = new ExcelWorkbook(poiWorkbook);
+        titleStyle = createCellStyle(IndexedColors.ROYAL_BLUE.index, true, false, false, 24);
+        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        headRowStyle = createCellStyle(null, true, false, false, null);
+        warningCellStyle = createCellStyle(IndexedColors.RED.index, true, false, false, null);
+        descriptionStyle = createCellStyle(IndexedColors.BLUE_GREY.index, false, true, true, null);
+    }
+
+    protected ExcelRow addDescriptionRow(String descriptionKey, int numberOfColumns) {
+        ExcelRow row = sheet.createRow();
+        row.createCell().setCellStyle(titleStyle).setCellValue("Merlin");
+        row.setHeight(50).addMergeRegion(0, numberOfColumns - 1);
+        row = sheet.createRow();
+        row.createCell().setCellStyle(descriptionStyle).setCellValue(I18n.getDefault().getMessage(descriptionKey)
+                + "\n"
+                + I18n.getDefault().getMessage("merlin.word.templating.sheet_configuration_hint"));
+        row.setHeight(80).addMergeRegion(0, numberOfColumns - 1);
+        return row;
+    }
+
+    protected ExcelCell addConfigRow(String variable, String value, String descriptionKey) {
+        ExcelRow row = sheet.createRow();
+        // Variable
+        row.createCell().setCellValue(variable);
+        // Value
+        row.createCell().setCellValue(value);
+        // Description
+        ExcelCell cell = row.createCell();
+        if (descriptionKey != null) {
+            cell.setCellValue(I18n.getDefault().getMessage(descriptionKey));
+        } else {
+            cell.setCellValue("");
+        }
+        return cell;
+    }
+
+    public static String getBooleanAsString(boolean value) {
+        return value ? "X" : "";
+    }
+
+    protected CellStyle createCellStyle(Short color, boolean bold, boolean italic, boolean wrapText, Integer fontSize) {
+        CellStyle style = workbook.getPOIWorkbook().createCellStyle();
+        Font font = workbook.getPOIWorkbook().createFont();
+        if (color != null) {
+            font.setColor(color);
+        }
+        font.setBold(bold);
+        font.setItalic(italic);
+        style.setFont(font);
+        if (wrapText) {
+            style.setWrapText(true);
+            style.setVerticalAlignment(VerticalAlignment.TOP);
+        }
+        if (fontSize != null) {
+            font.setFontHeightInPoints(fontSize.shortValue());
+        }
+        return style;
+    }
+
+    protected void writeValue(ExcelCell cell, Object value, VariableType type) {
+        Object targetValue = VariableDefinition.convertValue(value, type);
+        if (targetValue == null) {
+            return;
+        }
+        switch (type) {
+            case STRING:
+                cell.setCellValue((String) targetValue);
+                break;
+            case INT:
+                cell.setCellValue(workbook, (Integer)targetValue);
+                break;
+            case FLOAT:
+                cell.setCellValue(workbook, (Double)targetValue);
+                break;
+            case DATE:
+                log.error("Date not yet implemented.");
+        }
+    }
+
+
+    protected void createConfigurationSheet() {
+        sheet = workbook.createOrGetSheet("Configuration");
+        ExcelRow row = addDescriptionRow("merlin.word.templating.sheet_configuration_description", 3);
+        row = sheet.createRow();
+        row.createCells(headRowStyle, "Variable", "Value", "Description");
+    }
+}
--- merlin-core/src/main/java/de/reinhard/merlin/word/templating/TemplateDefinitionExcelWriter.java
+++ merlin-core/src/main/java/de/reinhard/merlin/word/templating/TemplateDefinitionExcelWriter.java
@@ -3,43 +3,32 @@ package de.reinhard.merlin.word.templating;
 import de.reinhard.merlin.I18n;
 import de.reinhard.merlin.excel.ExcelCell;
 import de.reinhard.merlin.excel.ExcelRow;
-import de.reinhard.merlin.excel.ExcelSheet;
 import de.reinhard.merlin.excel.ExcelWorkbook;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellAddress;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.stream.Collectors;
 
-public class TemplateDefinitionExcelWriter {
+public class TemplateDefinitionExcelWriter extends AbstractExcelWriter {
     private Logger log = LoggerFactory.getLogger(TemplateDefinitionExcelWriter.class);
 
-    private static final int COLUMN_WIDE_LENGTH = 5000;
-    private static final int COLUMN_EXTRA_WIDE_LENGTH = 15000;
-
-    private ExcelWorkbook workbook;
-    private CellStyle titleStyle;
-    private CellStyle headRowStyle;
-    private CellStyle warningCellStyle;
-    private CellStyle descriptionStyle;
-    private ExcelSheet sheet;
     private TemplateDefinition template;
 
     public ExcelWorkbook writeToWorkbook(TemplateDefinition template) {
         this.template = template;
-        Workbook poiWorkbook = new XSSFWorkbook();
-        workbook = new ExcelWorkbook(poiWorkbook);
-        titleStyle = createCellStyle(IndexedColors.ROYAL_BLUE.index, true, false, false, 24);
-        titleStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-        headRowStyle = createCellStyle(null, true, false, false, null);
-        warningCellStyle = createCellStyle(IndexedColors.RED.index, true, false, false, null);
-        descriptionStyle = createCellStyle(IndexedColors.BLUE_GREY.index, false, true, true, null);
+        super.init();
         createVariablesSheet();
         createDependentVariablesSheet();
         createConfigurationSheet();
+        addConfigRow("Name", template.getName(), null);
+        addConfigRow("Description", template.getDescription(), null);
+        addConfigRow("Filename", template.getFilenamePattern(), null);
+        ExcelCell cell = addConfigRow("Id", template.getId(), "merlin.word.templating.please_do_not_modify_id");
+        cell.setCellStyle(warningCellStyle);
+        sheet.autosize();
+        sheet.getPoiSheet().setActiveCell(new CellAddress(3, 0));
         return workbook;
     }
 
@@ -115,89 +104,4 @@ public class TemplateDefinitionExcelWriter {
         }
         sheet.getPoiSheet().setActiveCell(new CellAddress(3, 0));
     }
-
-    private void createConfigurationSheet() {
-        sheet = workbook.createOrGetSheet("Configuration");
-        ExcelRow row = addDescriptionRow("merlin.word.templating.sheet_configuration_description", 3);
-        row = sheet.createRow();
-        row.createCells(headRowStyle, "Variable", "Value", "Description");
-        addConfigRow("Name", template.getName(), null);
-        addConfigRow("Description", template.getDescription(), null);
-        addConfigRow("Filename", template.getFilenamePattern(), null);
-        ExcelCell cell = addConfigRow("Id", template.getId(), "merlin.word.templating.please_do_not_modify_id");
-        cell.setCellStyle(warningCellStyle);
-        sheet.autosize();
-        sheet.getPoiSheet().setActiveCell(new CellAddress(3, 0));
-    }
-
-    private ExcelRow addDescriptionRow(String descriptionKey, int numberOfColumns) {
-        ExcelRow row = sheet.createRow();
-        row.createCell().setCellStyle(titleStyle).setCellValue("Merlin");
-        row.setHeight(50).addMergeRegion(0, numberOfColumns - 1);
-        row = sheet.createRow();
-        row.createCell().setCellStyle(descriptionStyle).setCellValue(I18n.getDefault().getMessage(descriptionKey)
-                + "\n"
-                + I18n.getDefault().getMessage("merlin.word.templating.sheet_configuration_hint"));
-        row.setHeight(80).addMergeRegion(0, numberOfColumns - 1);
-        return row;
-    }
-
-    private ExcelCell addConfigRow(String variable, String value, String descriptionKey) {
-        ExcelRow row = sheet.createRow();
-        // Variable
-        row.createCell().setCellValue(variable);
-        // Value
-        row.createCell().setCellValue(value);
-        // Description
-        ExcelCell cell = row.createCell();
-        if (descriptionKey != null) {
-            cell.setCellValue(I18n.getDefault().getMessage(descriptionKey));
-        } else {
-            cell.setCellValue("");
-        }
-        return cell;
-    }
-
-    public static String getBooleanAsString(boolean value) {
-        return value ? "X" : "";
-    }
-
-    private CellStyle createCellStyle(Short color, boolean bold, boolean italic, boolean wrapText, Integer fontSize) {
-        CellStyle style = workbook.getPOIWorkbook().createCellStyle();
-        Font font = workbook.getPOIWorkbook().createFont();
-        if (color != null) {
-            font.setColor(color);
-        }
-        font.setBold(bold);
-        font.setItalic(italic);
-        style.setFont(font);
-        if (wrapText) {
-            style.setWrapText(true);
-            style.setVerticalAlignment(VerticalAlignment.TOP);
-        }
-        if (fontSize != null) {
-            font.setFontHeightInPoints(fontSize.shortValue());
-        }
-        return style;
-    }
-
-    private void writeValue(ExcelCell cell, Object value, VariableType type) {
-        Object targetValue = VariableDefinition.convertValue(value, type);
-        if (targetValue == null) {
-            return;
-        }
-        switch (type) {
-            case STRING:
-                cell.setCellValue((String) targetValue);
-                break;
-            case INT:
-                cell.setCellValue(workbook, (Integer)targetValue);
-                break;
-            case FLOAT:
-                cell.setCellValue(workbook, (Double)targetValue);
-                break;
-            case DATE:
-                log.error("Date not yet implemented.");
-        }
-    }
-}
+ }
