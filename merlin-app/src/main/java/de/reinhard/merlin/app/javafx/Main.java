4bf96472635368d6fa7f17b3ef988bbf2e0d8418
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-15 02:01:31 +0200

    Using reflection for calling Apple specific Java classes for avoiding compilation errors on non MacOS development systems.

--- merlin-app/src/main/java/de/reinhard/merlin/app/javafx/Main.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/javafx/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.apache.commons.lang.reflect.MethodUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,7 +54,15 @@ public class Main extends Application {
         if (RunningMode.getOSType() == RunningMode.OS_TYPE.MAC_OS) {
             URL iconURL = Main.class.getResource("/icon.png");
             Image image = new ImageIcon(iconURL).getImage();
-            com.apple.eawt.Application.getApplication().setDockIconImage(image);
+            // com.apple.eawt.Application.getApplication().setDockIconImage(image);
+            // Use reflection for avoiding compiling errors on non Mac OS development systems.
+            try {
+                Class<?> c = Class.forName("com.apple.eawt.Application");
+                Object application = MethodUtils.invokeStaticMethod(c, "getApplication", null);
+                MethodUtils.invokeMethod(application, "setDockIconImage", image);
+            } catch (Exception ex) {
+                log.error("Can't call com.apple.eawt.Application.getApplication().setDockIconImage(image): " + ex.getMessage(), ex);
+            }
         }
 
         Scene scene = new Scene(root);
