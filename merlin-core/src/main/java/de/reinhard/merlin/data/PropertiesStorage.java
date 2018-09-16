5b307f6df9bcbc51b72109bfc74743fade20cfc3
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-16 22:23:52 +0200

    Map<String, String> -> Map<String, Object>

--- merlin-core/src/main/java/de/reinhard/merlin/data/PropertiesStorage.java
+++ merlin-core/src/main/java/de/reinhard/merlin/data/PropertiesStorage.java
@@ -3,18 +3,29 @@ package de.reinhard.merlin.data;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * It's simply an {@link HashMap} filled by {@link de.reinhard.merlin.excel.ExcelConfigReader}.
+ */
 public class PropertiesStorage {
-    private Map<String, String> properties = new HashMap<String, String>();
+    private Map<String, Object> properties = new HashMap<>();
 
     public void setConfig(String key, String value) {
         properties.put(key, value);
     }
 
-    public String getConfig(String key) {
+    public Object getConfig(String key) {
         return properties.get(key);
     }
 
-    public Map<String, String> getConfig() {
+    public String getConfigString(String key) {
+        Object value = properties.get(key);
+        if (value == null) {
+            return null;
+        }
+        return String.valueOf(value);
+    }
+
+    public Map<String, Object> getConfig() {
         return properties;
     }
 
