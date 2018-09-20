303c5c2ce9feba535e38952a36772814816b8493
Fin Reinhard <fin.reinhard@icloud.com>
2018-09-20 23:01:58 +0200

    changed template state structure

--- merlin-webapp/src/reducers/template.js
+++ merlin-webapp/src/reducers/template.js
@@ -33,18 +33,28 @@ const reducer = (state = initialState, action) => {
             console.log(action);
 
             return Object.assign({}, state, {
-                isFetching: false,
-                failed: false,
-                loaded: true,
-                list: {...action.payload}
+               isFetching: false,
+               failed: false,
+               loaded: true,
+                // MAPS THE ARRAY OF TEMPLATES TO A 'HashMap' (id => template)
+               list: action.payload.reduce((accumulator, current) => ({
+                   ...accumulator,
+                   [current["id"]]: current
+               }), {})
             });
 
         // TODO HANDLE CASES
         case TEMPLATE_REQUESTED:
         case TEMPLATE_REQUEST_FAILED:
-        case TEMPLATE_RECEIVED:
-            console.log(action);
             return state;
+
+        case TEMPLATE_RECEIVED:
+            return {
+                ...state,
+                list: Object.assign({}, state.list, {
+                    [action.payload.id]: action.payload.template
+                })
+            };
 
         default:
             return state;
