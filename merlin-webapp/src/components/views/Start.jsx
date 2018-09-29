3fe99b26782b93404a4cfaa360f59a3e6bee92f0
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-29 13:03:04 +0200

    ToDo

--- merlin-webapp/src/components/views/Start.jsx
+++ merlin-webapp/src/components/views/Start.jsx
@@ -12,6 +12,7 @@ class Start extends React.Component {
 
                 <h3>ToDo</h3>
                 <ul>
+                    <li>Use Webpack for handling dev and production mode.</li>
                     <li>I18n: Get the translations from the server via json for labels etc. Do it globally or on every single view?</li>
                 </ul>
 
