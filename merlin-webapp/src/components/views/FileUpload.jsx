a9fb6299acead9d8f6b0169c1b7b50b24cd5844c
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-29 09:16:18 +0200

    Todo

--- merlin-webapp/src/components/views/FileUpload.jsx
+++ merlin-webapp/src/components/views/FileUpload.jsx
@@ -12,6 +12,19 @@ const FileUploadView = (props) => (
         <DropArea
             upload={props.uploadFile}
         />
+
+        <h3>ToDo</h3>
+        <ul>
+            <li>We should skip the upload button and do the action instantly after dropping a file.</li>
+            <li>Support the following upload-results:
+                <ol>
+                    <li>Response = Text: Display message (e. g.) error message such as "Unsupported file".</li>
+                    <li>Response = page: Redirect to a page with pre-filled data: a dialog asking the user what to do: Run a Template by choosing the matching templates, create a new one etc.</li>
+                    <li>Response = file: Download the returned file (zip, doc, xls etc.). This is used, if Merlin auto detects a run file and returns the result file directly</li>
+                </ol>
+            </li>
+        </ul>
+
     </div>
 );
 
