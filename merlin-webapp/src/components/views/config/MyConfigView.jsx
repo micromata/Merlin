9348930e7bf8ab4cb36c1463244cff6f781ac3ee
Kai Reinhard <K.Reinhard@micromata.de>
2018-09-29 23:45:15 +0200

    Works now.

--- merlin-webapp/src/components/views/config/MyConfigView.jsx
+++ merlin-webapp/src/components/views/config/MyConfigView.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 import {PageHeader} from 'react-bootstrap';
 import DirectoryItemsFieldset from "./DirectoryItemsFieldset";
+import {getRestServiceUrl} from "../../../actions/global";
 
 var directoryItems = [];
-directoryItems.push({index: 1, directory: "/Users/kai/Documents/", recursive: true});
-directoryItems.push({index: 2, directory: "/Users/kai/Documents/workspace/Merlin/templates", recursive: false});
-
 
 class MyConfigForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            port: this.props.port,
-            language: this.props.language,
-            directoryItems: this.props.directoryItems,
+            port: 8000,
+            language: 'en',
+            directoryItems: [],
             redirect: false
         }
         this.handleTextChange = this.handleTextChange.bind(this);
@@ -23,6 +21,38 @@ class MyConfigForm extends React.Component {
         this.removeDirectoryItem = this.removeDirectoryItem.bind(this);
         this.onSave = this.onSave.bind(this);
         this.onCancel = this.onCancel.bind(this);
+    }
+
+    componentDidMount() {
+        fetch(getRestServiceUrl("configuration/config"), {
+            method: "GET",
+            dataType: "JSON",
+            headers: {
+                "Content-Type": "text/plain; charset=utf-8",
+            }
+        })
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((data) => {
+                if (data.port) {
+                    this.setState({port: data.port});
+                }
+                if (data.language) {
+                    this.setState({language: data.language});
+                }
+                directoryItems.splice(0, directoryItems.length)
+                if (data.templatesDirs) {
+                    let idx = 0;
+                    data.templatesDirs.forEach(function (item) {
+                        directoryItems.push({index: idx++, directory: item.directory, recursive: item.recursive});
+                    });
+                    this.setState({directoryItems: directoryItems});
+                }
+            })
+            .catch((error) => {
+                console.log(error, "Oups, what's happened?")
+            })
     }
 
     setRedirect = () => {
@@ -58,7 +88,19 @@ class MyConfigForm extends React.Component {
     }
 
     onSave(event) {
-        console.log("onSave: " + JSON.stringify(this.state));
+        var config = {port: this.state.port, language: this.state.language, templatesDirs: []};
+        if (this.state.directoryItems) {
+            this.state.directoryItems.forEach(function (item) {
+                config.templatesDirs.push({directory: item.directory, recursive: item.recursive});
+            });
+        }
+        fetch(getRestServiceUrl("configuration/config"), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(config)
+        })
         this.setRedirect();
     }
 
@@ -82,7 +124,7 @@ class MyConfigForm extends React.Component {
 
     render() {
         if (this.state.redirect) {
-            return <Redirect to='/' />
+            return <Redirect to='/'/>
         }
         return (
             <form>
@@ -104,7 +146,7 @@ class MyConfigForm extends React.Component {
                         </select>
                     </div>
                 </div>
-                <DirectoryItemsFieldset items={this.props.directoryItems} addItem={this.addDirectoryItem}
+                <DirectoryItemsFieldset items={this.state.directoryItems} addItem={this.addDirectoryItem}
                                         removeItem={this.removeDirectoryItem}
                                         onDirectoryChange={this.handleDirectoryChange}
                                         onRecursiveFlagChange={this.handleRecursiveFlagChange}/>
@@ -129,15 +171,10 @@ class MyConfigView extends React.Component {
         return (
             <div>
                 <PageHeader>Config</PageHeader>
-                <MyConfigForm port="8042" language="German" directoryItems={directoryItems}/>
+                <MyConfigForm/>
 
                 <h3>ToDo</h3>
                 <ul>
-                    <li>Binding rest services.</li>
-                    <li>Submit-Button: Save changes via rest service.</li>
-                    <li>Submit-Button: Make Save button as default button (if the user hits return, this button
-                        should be executed).
-                    </li>
                     <li>Do the form validation (server and/or client side) with error fields.</li>
                 </ul>
 
