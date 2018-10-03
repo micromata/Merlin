cc1c7d681e39ec909014b372b2739268e262f8ad
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-03 16:15:56 +0200

    Examples renamed.

--- merlin-core/src/test/java/de/reinhard/merlin/word/templating/DirectoryScannerTest.java
+++ merlin-core/src/test/java/de/reinhard/merlin/word/templating/DirectoryScannerTest.java
@@ -1,24 +1,21 @@
 package de.reinhard.merlin.word.templating;
 
+import de.reinhard.merlin.Definitions;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DirectoryScannerTest {
-    private static final String TEST_TEMPLATES_DIR = "../examples/templates/";
-
     @Test
     public void scanTest() {
-        DirectoryScanner directoryScanner = new DirectoryScanner(new File(TEST_TEMPLATES_DIR), false);
+        DirectoryScanner directoryScanner = new DirectoryScanner(Definitions.EXAMPLES_TEMPLATES_TEST_DIR, false);
         directoryScanner.process();
         assertEquals(2, directoryScanner.getTemplateDefinitions().size());
         assertEquals("9MJdzFN2v2PKMJ9erj59", directoryScanner.getTemplateDefinitions().get(0).getId());
         assertEquals("JZpnpojeSuN5JDqtm9KZ", directoryScanner.getTemplateDefinitions().get(1).getId());
         assertEquals("LetterTemplate.xlsx",
                 directoryScanner.getTemplateDefinition("9MJdzFN2v2PKMJ9erj59").getFileDescriptor().getFilename());
-        assertEquals("ContractTemplate.xlsx",
+        assertEquals("EmploymentContractTemplate.xlsx",
                 directoryScanner.getTemplateDefinition("JZpnpojeSuN5JDqtm9KZ").getFileDescriptor().getFilename());
     }
 }
