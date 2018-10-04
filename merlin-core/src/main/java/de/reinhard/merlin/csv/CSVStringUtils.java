9175b22bfac4cb3aa1aae1eedff12be7cc931fad
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-04 10:35:14 +0200

    New quote chars added, which are now supported to be used in Word-templates.

--- merlin-core/src/main/java/de/reinhard/merlin/csv/CSVStringUtils.java
+++ merlin-core/src/main/java/de/reinhard/merlin/csv/CSVStringUtils.java
@@ -13,10 +13,10 @@ public class CSVStringUtils {
 
     ;
 
-    private static final String VALUE_SINGLE_START_QUOTS = "‚'";
-    private static final String VALUE_SINGLE_END_QUOTS = "‘'";
-    private static final String VALUE_DOUBLE_START_QUOTS = "„\"“";
-    private static final String VALUE_DOUBLE_END_QUOTS = "“\"";
+    private static final String VALUE_SINGLE_START_QUOTS = "‚'’";
+    private static final String VALUE_SINGLE_END_QUOTS = "‘'’";
+    private static final String VALUE_DOUBLE_START_QUOTS = "„\"“”";
+    private static final String VALUE_DOUBLE_END_QUOTS = "“”\"";
     private static final String VALUE_SEPARATOR_CHARS = ",;,:";
 
     public static String[] parseStringList(String str) {
