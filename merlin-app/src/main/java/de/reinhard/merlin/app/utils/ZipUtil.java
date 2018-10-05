f4dd65e72e80b027f346c96114646c7176b92fd4
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-05 08:01:57 +0200

    Filecounter added.

--- merlin-app/src/main/java/de/reinhard/merlin/app/utils/ZipUtil.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/utils/ZipUtil.java
@@ -1,10 +1,13 @@
 package de.reinhard.merlin.app.utils;
 
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -13,6 +16,7 @@ public class ZipUtil {
     private ZipOutputStream zipOut;
     private ByteArrayOutputStream outStream;
     private String filename;
+    private Map<String, Integer> usedFilenames = new HashMap<>();
 
     public ZipUtil(String zipFilename) {
         this.filename = zipFilename;
@@ -20,8 +24,25 @@ public class ZipUtil {
         zipOut = new ZipOutputStream(outStream);
     }
 
+    /**
+     * For multiple filenames, the filenames will be modified in a MacOS X style:
+     * <tt>Filename.txt, Filename-2.txt, Filename-2.txt,</tt> ...
+     *
+     * @param filename
+     * @param content
+     */
     public void addZipEntry(String filename, byte[] content) {
         try {
+            Integer counter = usedFilenames.get(filename);
+            if (counter != null) {
+                // Modify multiples filenames by adding counter (MacOS style).
+                filename = FilenameUtils.getFullPath(filename)
+                        + FilenameUtils.getBaseName(filename)
+                        + ++counter + FilenameUtils.getExtension(filename);
+            } else {
+                counter = 1;
+            }
+            usedFilenames.put(filename, counter);
             ZipEntry zipEntry = new ZipEntry(filename);
             zipOut.putNextEntry(zipEntry);
             zipOut.write(content);
