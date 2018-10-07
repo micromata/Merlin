3380566c3c7032db70b27a4a1cbc54e702964dcc
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-07 20:02:02 +0200

    Test case fixed (for new WatchService)

--- merlin-app/src/main/java/de/reinhard/merlin/app/storage/TestData.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/storage/TestData.java
@@ -13,9 +13,15 @@ public class TestData {
     private static Logger log = LoggerFactory.getLogger(TestData.class);
 
     private static final String TEST_TEMPLATES_DIR = "examples/templates";
+    private static boolean created;
 
     public static void create(File parent) {
-        File dir = new File(parent, TEST_TEMPLATES_DIR);
+        if (created) {
+            return; // Don't create twice.
+        }
+        created = true;
+        File dir = new File(parent, TEST_TEMPLATES_DIR).getAbsoluteFile();
+        log.info("Creating test data from '" + dir.toPath());
         DirectoryScanner directoryScanner = new DirectoryScanner(dir.toPath(), false);
         if (directoryScanner.getTemplateDefinitions() == null) {
             log.error("Can't scan directory '" + dir.getAbsolutePath() + "' for test template files.");
