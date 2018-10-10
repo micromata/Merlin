3b32c3268d3a4d26cd3393e3ea846fa531994779
Fin Reinhard <fin.reinhard@icloud.com>
2018-10-10 21:46:08 +0200

    beautified global.js

--- merlin-webapp/src/actions/global.js
+++ merlin-webapp/src/actions/global.js
@@ -3,15 +3,11 @@
 global.testserver = 'http://localhost:8042';
 global.restBaseUrl = (process.env.NODE_ENV === 'development' ? global.testserver : '') + '/rest';
 
-export function getRestServiceUrl(restService) {
-    return global.restBaseUrl + '/' + restService;
-}
+export const getRestServiceUrl = restService => `${global.restBaseUrl}/${restService}`;
 
 export const getResponseHeaderFilename = contentDisposition => {
     const matches = /filename[^;=\n]*=(UTF-8(['"]*))?(.*)/.exec(contentDisposition);
     return matches.length >= 3 && matches[3] ? decodeURI(matches[3].replace(/['"]/g, '')) : 'download';
 };
 
-export function revisedRandId() {
-    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
-}
+export const revisedRandId = () => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
