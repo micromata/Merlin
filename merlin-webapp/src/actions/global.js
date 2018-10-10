66f81f1069bcc63fb1e38df15732e5b970218885
Fin Reinhard <fin.reinhard@icloud.com>
2018-10-10 21:56:34 +0200

    reformatted Template Component

--- merlin-webapp/src/actions/global.js
+++ merlin-webapp/src/actions/global.js
@@ -7,7 +7,7 @@ export const getRestServiceUrl = restService => `${global.restBaseUrl}/${restSer
 
 export const getResponseHeaderFilename = contentDisposition => {
     const matches = /filename[^;=\n]*=(UTF-8(['"]*))?(.*)/.exec(contentDisposition);
-    return matches.length >= 3 && matches[3] ? decodeURI(matches[3].replace(/['"]/g, '')) : 'download';
+    return matches && matches.length >= 3 && matches[3] ? decodeURI(matches[3].replace(/['"]/g, '')) : 'download';
 };
 
 export const revisedRandId = () => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
