0f71b5fef6e8474fc4f7a010fe1f74002bd30267
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-12 19:11:06 +0200

    Migration...

--- merlin-webapp/src/components/general/Menu.jsx
+++ merlin-webapp/src/components/general/Menu.jsx
@@ -1,44 +1,68 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Nav, NavLink, Navbar, NavItem} from 'reactstrap';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
 
 class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 
     render() {
-
         return (
-            <div>Test</div>
-/*            <Navbar collapseOnSelect>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link to={'/'}>
-                            Merlin Webapp
-                        </Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle/>
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Merlin</NavbarBrand>
+                <NavbarToggler onClick={this.toggle}/>
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
                         {
                             this.props.routes.map((route, index) => (
-                                <NavLink
-                                    key={index}
-                                    href={route[1]}
-                                    exact
-                                >
-                                    <NavItem
-                                        eventKey={index + 1}
-                                    >
-                                        {route[0]}
-                                    </NavItem>
-                                </NavLink>
+                                <NavItem key={index}>
+                                    <NavLink href={route[1]}>{route[0]}</NavLink>
+                                </NavItem>
                             ))
                         }
                     </Nav>
-                </Navbar.Collapse>
-            </Navbar>*/
+                </Collapse>
+            </Navbar>
         );
     }
+
+    /* <UncontrolledDropdown nav inNavbar>
+         <DropdownToggle nav caret>
+             Options
+         </DropdownToggle>
+         <DropdownMenu right>
+             <DropdownItem>
+                 Option 1
+             </DropdownItem>
+             <DropdownItem>
+                 Option 2
+             </DropdownItem>
+             <DropdownItem divider />
+             <DropdownItem>
+                 Reset
+             </DropdownItem>
+         </DropdownMenu>
+     </UncontrolledDropdown>*/
 }
 
 export default Menu;
