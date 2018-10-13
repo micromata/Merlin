9d90b6c814a728deab030036000dfc3088bbe1ac
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-13 08:03:44 +0200

    Unused stuff removed.

--- merlin-webapp/src/components/general/IconComponents.jsx
+++ merlin-webapp/src/components/general/IconComponents.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {faCheckSquare, faCoffee} from '@fortawesome/free-solid-svg-icons';
 import {
     faCheck,
     faPlus,
