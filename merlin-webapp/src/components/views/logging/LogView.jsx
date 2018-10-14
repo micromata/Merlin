be5138f484838f5c697f7e4521697aedcb3a155b
Fin Reinhard <fin.reinhard@icloud.com>
2018-10-14 19:22:07 +0200

    removed old LogViewer

--- merlin-webapp/src/components/views/logging/LogView.jsx
+++ merlin-webapp/src/components/views/logging/LogView.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 import {PageHeader} from '../../general/BootstrapComponents';
 import LogFilters from './LogFilters';
 import {changeFilter, requestLogReload} from '../../../actions';
 import LogTable from './LogTable';
-import PropTypes from 'prop-types';
+import './LogViewer.css';
 
 class LogView extends React.Component {
 
