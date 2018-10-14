966e306a075045e8e9987e36a789062efe612115
Fin Reinhard <fin.reinhard@icloud.com>
2018-10-14 20:58:08 +0200

    added localStorage Redux feature

--- merlin-webapp/src/index.js
+++ merlin-webapp/src/index.js
@@ -9,10 +9,17 @@ import WebApp from './containers/WebApp';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from './reducers';
 
+let storedState = window.localStorage.getItem('state');
+
 const store = createStore(
     reducers,
+    storedState ? JSON.parse(storedState) : undefined,
     applyMiddleware(thunk)
 );
+
+store.subscribe(() => {
+    window.localStorage.setItem('state', JSON.stringify(store.getState()));
+});
 
 ReactDOM.render(
     <Provider store={store}>
