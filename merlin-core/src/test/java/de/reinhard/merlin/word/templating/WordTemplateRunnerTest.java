1d6c71c245f9b3b63718f8cf99a799379f078d46
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-16 00:24:36 +0200

    Remove legalaffaires test.

--- merlin-core/src/test/java/de/reinhard/merlin/word/templating/WordTemplateRunnerTest.java
+++ merlin-core/src/test/java/de/reinhard/merlin/word/templating/WordTemplateRunnerTest.java
@@ -1,7 +1,6 @@
 package de.reinhard.merlin.word.templating;
 
 import de.reinhard.merlin.Definitions;
-import de.reinhard.merlin.excel.ExcelWorkbook;
 import de.reinhard.merlin.word.WordDocument;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
@@ -13,15 +12,11 @@ import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WordTemplateRunnerTest {
     private Logger log = LoggerFactory.getLogger(WordTemplateRunnerTest.class);
-
-    private final static String MY_WORKING_DIR = "/Users/kai/workspace/Micromata/MicromataV3/legalaffairs/Templates";
-
+    
     @Test
     public void readWordTest() throws Exception {
         TemplateDefinition templateDefinition = DefinitionExcelConverterTest.create();
@@ -37,41 +32,6 @@ public class WordTemplateRunnerTest {
                 "4/1/2001", "25", "30");
         runDocument(runner, "Bärta Üßten", "female", "8/14/2017",
                 "19/1/2017", "40", "30");
-        runMyFiles();
-    }
-
-    private void runMyFiles() throws Exception {
-        File dir = new File(MY_WORKING_DIR);
-        if (!dir.exists()) {
-            return;
-        }
-        log.info("Processing my-working-dir: " + MY_WORKING_DIR);
-        WordDocument templateDocument = new WordDocument(new File(MY_WORKING_DIR, "Arbeitsvertrag-Test-Template.docx"));
-        ExcelWorkbook workbook = new ExcelWorkbook(new File(MY_WORKING_DIR, "Arbeitsvertrag-Test-Template.xlsx"));
-        TemplateDefinitionExcelReader reader = new TemplateDefinitionExcelReader();
-        TemplateDefinition templateDefinition = reader.readFromWorkbook(workbook);
-        WordTemplateRunner runner = new WordTemplateRunner(templateDefinition, templateDocument);
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("Mitarbeiter", "Markus Meier");
-        variables.put("Geschlecht", "männlich");
-        variables.put("MA_Strasse", "ABC-Straße 5");
-        variables.put("MA_PLZ", "12345");
-        variables.put("MA_Ort", "Kassel");
-        variables.put("Vertragstyp", "unbefristet");
-        variables.put("Vertragsbeginn", "01.04.2017");
-        variables.put("Vertragsende", "---");
-        variables.put("Wochenstunden", "40");
-        variables.put("Urlaubstage", "30");
-        variables.put("Position", "Legalmitarbeiter");
-        variables.put("Aufgaben", "- Rechtliche Fragen rund um lorem epsum\n- Rechtliche Belange im Allgemeinen\n- Rechtliches im Speziellen.");
-        runDocument(runner, variables);
-        variables.put("Mitarbeiter", "Sandra Schmidt");
-        variables.put("Geschlecht", "weiblich");
-        variables.put("Wochenstunden", "20");
-        variables.put("Vertragstyp", "befristet");
-        variables.put("Vertragsende", "31.08.2017");
-        variables.put("Position", "Legalmitarbeiterin");
-        runDocument(runner, variables);
     }
 
     private void runDocument(WordTemplateRunner runner, String employee, String gender, String date,
