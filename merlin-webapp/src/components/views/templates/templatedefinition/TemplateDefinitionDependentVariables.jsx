e185bcfc5e1afe29885d5b6e99b1fc653fed8361
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-16 23:58:58 +0200

    Display properties of dependent variables.

--- merlin-webapp/src/components/views/templates/templatedefinition/TemplateDefinitionDependentVariables.jsx
+++ merlin-webapp/src/components/views/templates/templatedefinition/TemplateDefinitionDependentVariables.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Form, Table} from "reactstrap";
+import EditableTextField from "../../../general/forms/EditableTextField";
 
 class TemplateDefinitionDependentVariables extends React.Component {
     render = () => {
@@ -6,13 +8,43 @@ class TemplateDefinitionDependentVariables extends React.Component {
             return null;
         }
         const rows = [];
-        this.props.definition.dependentVariableDefinitions.forEach((variable) => {
+        this.props.definition.dependentVariableDefinitions.forEach((variable, index) => {
             rows.push(
-                <div key={variable.name}>{variable.name}</div>
+                <tr key={variable.name}>
+                    <td>
+                        <EditableTextField
+                            type={'text'}
+                            value={variable.name}
+                            name={'name'}
+                            onChange={this.props.handleTextChange}
+                            index={index}/>
+                    </td>
+                    <td>{variable.dependsOn.name}</td>
+                    <td>{variable.mappingList.join(', ')}</td>
+                    <td>
+                        <ul>{Object.keys(variable.mapping).map((key, index) => {
+                            return (<li key={index}>{`${key} => ${variable.mapping[key]}`}</li>);
+                        })}</ul>
+                    </td>
+                </tr>
             );
         });
         return <React.Fragment>
-            {rows}
+            <Form>
+                <Table striped responsive bordered hover>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Depends on</th>
+                        <th>Mapping list</th>
+                        <th>Mapping</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {rows}
+                    </tbody>
+                </Table>
+            </Form>
         </React.Fragment>;
     };
 }
