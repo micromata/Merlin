d0ab96cdfc0fada7386677d1bff56ad7802a05c0
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-20 19:40:37 +0200

    update: Check localhost.

--- merlin-app/src/main/java/de/reinhard/merlin/app/rest/UpdateServiceRest.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/rest/UpdateServiceRest.java
@@ -4,9 +4,11 @@ import de.reinhard.merlin.app.updater.AppUpdater;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 @Path("/updates")
@@ -16,7 +18,12 @@ public class UpdateServiceRest {
     @GET
     @Path("/install")
     @Produces(MediaType.TEXT_PLAIN)
-    public String downloadUpdate() {
+    public String downloadUpdate(@Context HttpServletRequest requestContext) {
+        String msg = FilesServiceRest.checkLocalDesktopAvailable(requestContext);
+        if (msg != null) {
+            log.error("Can't launch updater from remote client (only from localhost): " + msg);
+            return msg;
+        }
         boolean result = AppUpdater.getInstance().install();
         return result ? "OK" : "ERROR";
     }
