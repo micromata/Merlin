f71729b10bf8d73960b2e3b62c5f4736efd54c19
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-20 08:31:15 +0200

    Unused code removed.

--- merlin-app/src/main/java/de/reinhard/merlin/app/storage/Storage.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/storage/Storage.java
@@ -4,19 +4,13 @@ import de.reinhard.merlin.app.ConfigurationHandler;
 import de.reinhard.merlin.app.ConfigurationListener;
 import de.reinhard.merlin.app.ConfigurationTemplatesDir;
 import de.reinhard.merlin.app.javafx.RunningMode;
-import de.reinhard.merlin.excel.ExcelWorkbook;
-import de.reinhard.merlin.persistency.FileDescriptor;
-import de.reinhard.merlin.persistency.PersistencyRegistry;
 import de.reinhard.merlin.persistency.templates.DirectoryScanner;
 import de.reinhard.merlin.word.templating.Template;
 import de.reinhard.merlin.word.templating.TemplateDefinition;
-import de.reinhard.merlin.word.templating.TemplateDefinitionExcelReader;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -39,10 +33,6 @@ public class Storage implements ConfigurationListener {
     private Storage() {
         directoryScannerMap = new HashMap<>();
         ConfigurationHandler.getInstance().register(this);
-    }
-
-    public void clear() {
-        dirty = true;
     }
 
     public void add(DirectoryScanner directoryScanner) {
@@ -68,64 +58,12 @@ public class Storage implements ConfigurationListener {
         for (DirectoryScanner directoryScanner : directoryScannerMap.values()) {
             TemplateDefinition templateDefinition = directoryScanner.getTemplateDefinition(idOrPrimaryKey);
             if (templateDefinition != null) {
-                reloadIfModified(templateDefinition);
                 return templateDefinition;
             }
         }
         log.error("Template definition with id or primary key '" + idOrPrimaryKey + "' not found.");
         return null;
     }
-
-    private void reloadIfModified(List<TemplateDefinition> templateDefinitions) {
-        checkRefresh();
-        if (CollectionUtils.isEmpty(templateDefinitions)) {
-            // Nothing to do;
-            return;
-        }
-        for (TemplateDefinition templateDefinition : templateDefinitions) {
-            reloadIfModified(templateDefinition);
-        }
-    }
-
-    private void reloadIfModified(TemplateDefinition templateDefinition) {
-        checkRefresh();
-        FileDescriptor descriptor = templateDefinition.getFileDescriptor();
-        if (descriptor == null) {
-            log.warn("No file descriptor given, can't check modification of template definition: '" + templateDefinition.getId() + "'.");
-            return;
-        }
-        Path path = descriptor.getCanonicalPath();
-        if (!PersistencyRegistry.getDefault().exists(path)) {
-            log.warn("File '" + path.toAbsolutePath() + "' doesn't exist, can't check modification of template definition: '" + templateDefinition.getId() + "'.");
-            return;
-        }
-        if (descriptor.isModified(path)) {
-            log.info("Template definition file '" + templateDefinition.getId() + "' modified. Reload from file: " + path.toAbsolutePath());
-            TemplateDefinitionExcelReader reader = new TemplateDefinitionExcelReader();
-            ExcelWorkbook workbook = ExcelWorkbook.create(path);
-            templateDefinition = reader.readFromWorkbook(workbook);
-            //templateDefinitionsByDirectoryAndId.put()
-        }
-    }
-
-   /* @Override
-    public void putTemplateDefinition(FileDescriptor fileDescriptor, TemplateDefinition templateDefinition) {
-        String directory = "/";
-        if (fileDescriptor != null && StringUtils.isNotEmpty(fileDescriptor.getDirectory())) {
-            directory = fileDescriptor.getDirectory();
-        }
-        Map<String, TemplateDefinition> templateDefinitionsMap = templateDefinitionsByDirectoryAndId.get(directory);
-        if (templateDefinitionsMap == null) {
-            templateDefinitionsMap = new HashMap<>();
-            templateDefinitionsByDirectoryAndId.put(directory, templateDefinitionsMap);
-        }
-        templateDefinitionsMap.put(normalizeTemplateId(templateDefinition.getId()), templateDefinition);
-    }
-
-    @Override
-    public void putTemplate(String canonicalPath, Template template) {
-        templatesByCanonicalPath.put(normalizeCanonicalPath(canonicalPath), template);
-    }*/
 
     public List<Template> getAllTemplates() {
         checkRefresh();
