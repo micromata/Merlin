5d6ea82769fa7b697427d5cbeb6d1b1863fb0cfa
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-25 00:36:03 +0200

    FilesServiceRest divided in several files.

--- merlin-app/src/main/java/de/reinhard/merlin/app/rest/FilesServiceRest.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/rest/FilesServiceRest.java
@@ -1,54 +1,20 @@
 package de.reinhard.merlin.app.rest;
 
-import de.reinhard.merlin.app.javafx.FileSystemBrowser;
-import de.reinhard.merlin.app.javafx.RunningMode;
-import de.reinhard.merlin.app.storage.Storage;
-import de.reinhard.merlin.persistency.FileDescriptor;
-import de.reinhard.merlin.word.templating.Template;
-import de.reinhard.merlin.word.templating.TemplateDefinition;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.glassfish.jersey.media.multipart.ContentDisposition;
-import org.glassfish.jersey.media.multipart.FormDataBodyPart;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
-import java.awt.*;
 import java.io.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @Path("/files")
 public class FilesServiceRest {
     private Logger log = LoggerFactory.getLogger(FilesServiceRest.class);
     private static final String TEST_OUT_DIR = "./merlin-core/out/";
     private static final String TEST_SRC_DIR = "./merlin-core/examples/tests/";
-
-    // https://www.geekmj.org/jersey/jax-rs-multiple-files-upload-example-408/
-
-    @POST
-    @Path("/upload")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(FormDataMultiPart form) {
-
-        FormDataBodyPart filePart = form.getField("file");
-
-        ContentDisposition headerOfFilePart = filePart.getContentDisposition();
-
-        InputStream fileInputStream = filePart.getValueAs(InputStream.class);
-
-        // save the file to the server
-        saveFile(fileInputStream, new File(TEST_OUT_DIR), headerOfFilePart.getFileName());
-        String output = "File upload OK.";
-        return Response.status(200).entity(output).build();
-    }
 
     @GET
     @Path("/download")
@@ -62,103 +28,6 @@ public class FilesServiceRest {
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response downloadFilebyPath(@PathParam("filename") String fileName) {
         return download(fileName);
-    }
-
-    /**
-     * Opens a directory browser or file browser on the desktop app and returns the chosen dir/file. Works only if Browser and Desktop app are running
-     * on the same host.
-     *
-     * @param type    Supported values: "dir", "directory", "excel", "xls", "word", "doc", "file"
-     * @param current The current path of file. If not given the directory/file browser starts with the last used directory or user.home.
-     * @return The chosen directory path (absolute path).
-     */
-    @GET
-    @Path("/browse-local-filesystem")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String browseLocalFilesystem(@Context HttpServletRequest requestContext, @QueryParam("type") String type, @QueryParam("current") String current) {
-        String msg = checkLocalDesktopAvailable(requestContext);
-        if (msg != null) {
-            return msg;
-        }
-        FileSystemBrowser.SelectFilter filter = FileSystemBrowser.getFilter(type);
-        CompletableFuture<File> future = new CompletableFuture<>();
-        File initialDirectory = null;
-        if (StringUtils.isNotBlank(current)) {
-            initialDirectory = FileSystemBrowser.getDirectory(new File(current));
-        }
-        FileSystemBrowser.getInstance().open(filter, initialDirectory, future);
-        File file = null;
-        try {
-            file = future.get(); // wait for future to be assigned a result and retrieve it
-        } catch (InterruptedException | ExecutionException ex) {
-            log.error("While waiting for file browser: " + ex.getMessage(), ex);
-        }
-        FileSystemBrowser.getInstance().setLastDir(file);
-        String result = "{\"directory\":\"" + (file != null ? file.getAbsolutePath() : "") + "\"}";
-        return result;
-    }
-
-    /**
-     * @return OK, if the local desktop services such as open file browser etc. are available.
-     */
-    @GET
-    @Path("/local-fileservices-available")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String browseLocalFilesystem(@Context HttpServletRequest requestContext) {
-        String msg = checkLocalDesktopAvailable(requestContext);
-        if (msg != null) {
-            return msg;
-        }
-        return "OK";
-    }
-
-    /**
-     * Works only if the client and the server are running on localhost and the Desktop
-     *
-     * @param primaryKey File to open on local file system.
-     * @return OK, if the Desktop services are available and .
-     */
-    @GET
-    @Path("/open-local-file")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String openLocalFile(@Context HttpServletRequest requestContext, @QueryParam("primaryKey") String primaryKey) {
-        if (StringUtils.isBlank(primaryKey)) {
-            return "Can't open file, primaryKey required by this service.";
-        }
-        String msg = checkLocalDesktopAvailable(requestContext);
-        if (msg != null) {
-            return msg;
-        }
-        if (!Desktop.isDesktopSupported()) {
-            msg = "Desktop service isn't supported.";
-            log.error(msg);
-            return msg;
-        }
-        java.nio.file.Path path;
-        FileDescriptor fileDescriptor = null;
-        TemplateDefinition templateDefinition = Storage.getInstance().getTemplateDefinition(primaryKey);
-        if (templateDefinition != null) {
-            fileDescriptor = templateDefinition.getFileDescriptor();
-        } else {
-            Template template = Storage.getInstance().getTemplate(primaryKey);
-            if (template != null) {
-                fileDescriptor = template.getFileDescriptor();
-            }
-        }
-        if (fileDescriptor == null) {
-            msg = "File represented by primaryKey '" + primaryKey + "' not found";
-            log.error(msg);
-            return msg;
-        }
-        File file = fileDescriptor.getCanonicalPath().toFile();
-        try {
-            Desktop desktop = Desktop.getDesktop();
-            desktop.open(file);
-        } catch (IOException ex) {
-            log.error("Error while opening file '" + file.getAbsolutePath() + "'");
-            return "Error while trying to open file '" + file.getAbsolutePath() + "'.";
-        }
-        return "OK";
     }
 
     // save uploaded file to a defined location on the server
@@ -200,20 +69,5 @@ public class FilesServiceRest {
         }
 
         return response;
-    }
-
-    /**
-     * @return null, if the local app (JavaFX) is running and the request is from localhost. Otherwise message, why local
-     * service isn't available.
-     */
-    static String checkLocalDesktopAvailable(HttpServletRequest requestContext) {
-        if (RunningMode.isRunning() == false) {
-            return "Service unavailable. No desktop app on localhost available.";
-        }
-        String remoteAddr = requestContext.getRemoteAddr();
-        if (remoteAddr == null || !remoteAddr.equals("127.0.0.1")) {
-            return "Service not available. Can't call this service remote. Run this service on localhost of the running desktop app.";
-        }
-        return null;
     }
 }
--- merlin-app/src/main/java/de/reinhard/merlin/app/rest/RestUtils.java
+++ merlin-app/src/main/java/de/reinhard/merlin/app/rest/RestUtils.java
@@ -1,10 +1,27 @@
 package de.reinhard.merlin.app.rest;
 
+import de.reinhard.merlin.app.javafx.RunningMode;
 import org.slf4j.Logger;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 
 public class RestUtils {
+
+    /**
+     * @return null, if the local app (JavaFX) is running and the request is from localhost. Otherwise message, why local
+     * service isn't available.
+     */
+    static String checkLocalDesktopAvailable(HttpServletRequest requestContext) {
+        if (RunningMode.isRunning() == false) {
+            return "Service unavailable. No desktop app on localhost available.";
+        }
+        String remoteAddr = requestContext.getRemoteAddr();
+        if (remoteAddr == null || !remoteAddr.equals("127.0.0.1")) {
+            return "Service not available. Can't call this service remote. Run this service on localhost of the running desktop app.";
+        }
+        return null;
+    }
 
     static Response get404Response(Logger log, String errorMessage) {
         log.error(errorMessage);
