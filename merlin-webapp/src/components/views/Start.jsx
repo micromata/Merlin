608f2be75c43fdfb49474b19a7a3354b407ca78e
Kai Reinhard <K.Reinhard@micromata.de>
2018-10-31 22:54:42 +0100

    Welcome screen.

--- merlin-webapp/src/components/views/Start.jsx
+++ merlin-webapp/src/components/views/Start.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {PageHeader} from '../general/BootstrapComponents';
+import I18n from "../general/translation/I18n";
 
 class Start extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <PageHeader>
-                    Start
+                    <I18n name={'startscreen.welcome.title'}/>
                 </PageHeader>
+                <div class="welcome-intro"><I18n name={'startscreen.welcome.text'}/></div>
+                <div class="welcome-enjoy"><I18n name={'startscreen.welcome.enjoy'}/></div>
+                <div class="welcome-documentation-link"><a href={'/docs/index.html'} target="_blank" rel="noopener noreferrer"><I18n name={'startscreen.welcome.documentation'}/></a></div>
             </React.Fragment>
         );
     }
