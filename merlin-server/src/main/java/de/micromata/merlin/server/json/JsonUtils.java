aab97925f30fa5692727208d04bc7886889b5180
Kai Reinhard <K.Reinhard@micromata.de>
2018-11-11 13:33:42 +0100

    Uses now StringEscapeUtils of apache.commons.text.

--- merlin-server/src/main/java/de/micromata/merlin/server/json/JsonUtils.java
+++ merlin-server/src/main/java/de/micromata/merlin/server/json/JsonUtils.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
