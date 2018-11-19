9daffc9b1f9eec4c163fdb021e76ab238e577dd3
Kai Reinhard <K.Reinhard@micromata.de>
2018-11-19 21:27:44 +0100

    ...

--- merlin-paypal/src/test/java/de/micromata/merlin/paypal/PaymentTest.java
+++ merlin-paypal/src/test/java/de/micromata/merlin/paypal/PaymentTest.java
@@ -16,14 +16,24 @@ class PaymentTest {
         PayPalConfig config = PayPalConfig.createDemoConfig();
         Payment payment = new Payment();
         Transaction transaction = new Transaction();
+        transaction.setInoviceNumber(generateString(200));
+        assertEquals(127, transaction.getInoviceNumber().length());
+        assertEquals(generateString(124) + "...", transaction.getInoviceNumber());
         transaction.addItem("Online Elections 2019", 29.99);
         Details details = new Details().setTax(5.70);
         transaction.createAmount(Amount.Currency.EUR, details);
-        transaction.setInoviceNumber("1234");
         payment.addTransaction(transaction).setNoteToPayer("Enjoy your Elections with POLYAS.");
         payment.setConfig(config);
         payment.recalculate();
         assertEquals("35.69", transaction.getAmount().getTotal().toString());
         System.out.println(JsonUtils.toJson(payment, true));
+    }
+
+    private String generateString(int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(i % 10);
+        }
+        return sb.toString();
     }
 }
