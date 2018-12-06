918321c96e2a58baee77c86773014316e9e022d3
Kai Reinhard <K.Reinhard@micromata.de>
2018-12-06 01:31:37 +0100

    i18n-converter...

---
+++ merlin-i18n-converter/src/main/java/de/micromata/merlin/excel/i18n/I18nConverter.java
@@ -0,0 +1,52 @@
+package de.micromata.merlin.excel.i18n;
+
+import lombok.Getter;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+
+public class I18nConverter {
+    private static Logger log = LoggerFactory.getLogger(I18nConverter.class);
+
+    @Getter
+    private Translations translations;
+
+    public I18nConverter() {
+        this.translations = new Translations();
+    }
+
+    public I18nConverter(Translations translations) {
+        this.translations = translations;
+    }
+
+    public void importTranslations(File file) throws IOException {
+        if (!file.exists()) {
+            log.error("File '" + file.getAbsolutePath() + "' doesn't exist. Skipping.");
+            return;
+        }
+        translations = new Translations();
+        translations.setCreateKeyIfNotPresent(true);
+        translations.setOverwriteExistingTranslations(false);
+        String extension = FilenameUtils.getExtension(file.getName()).toLowerCase();
+        if (extension.startsWith("xls")) {
+            I18nExcelConverter i18nExcelConverter = new I18nExcelConverter(translations);
+            try (InputStream intputStream = new FileInputStream(file)) {
+                i18nExcelConverter.importTranslations(intputStream, file.getName());
+            }
+            return;
+        }
+        if (extension.startsWith("js")) {
+            I18nJsonConverter jsonConverter = new I18nJsonConverter(translations);
+            try (Reader reader = new FileReader(file)) {
+                jsonConverter.importTranslations(reader);
+            }
+            return;
+        }
+        I18nPropertiesConverter i18nPropertiesConverter = new I18nPropertiesConverter(translations);
+        try (Reader reader= new FileReader(file)) {
+            i18nPropertiesConverter.importTranslations("en", reader);
+        }
+    }
+}
---
+++ merlin-i18n-converter/src/main/java/de/micromata/merlin/excel/i18n/Main.java
@@ -0,0 +1,55 @@
+package de.micromata.merlin.excel.i18n;
+
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static final Main main = new Main();
+
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        main._start(args);
+    }
+
+    private void _start(String[] args) {
+        // create Options object
+        Options options = new Options();
+        options.addOption("o", "output", true, "The base name of the output files. Default is 'i18n.'.");
+        options.addOption("h", "help", false, "Print this help screen.");
+        options.addOption("p", "properties", true, "Read a property file‚");
+        CommandLineParser parser = new DefaultParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(options, args);
+            if (line.hasOption('h')) {
+                printHelp(options);
+                return;
+            }
+            String basename = "i18n.";
+            if (line.hasOption('o')) {
+                basename = line.getOptionValue("o");
+            }
+            String[] files = line.getArgs();
+            if (files == null ||files.length == 0) {
+                printHelp(options);
+                return;
+            }
+
+        } catch (ParseException ex) {
+            // oops, something went wrong
+            System.err.println("Parsing failed.  Reason: " + ex.getMessage());
+            printHelp(options);
+        }
+    }
+
+
+    private static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("merlin-i18n-converter [OPTIONS] [FILE1] [FILE2]...", options);
+    }
+}
