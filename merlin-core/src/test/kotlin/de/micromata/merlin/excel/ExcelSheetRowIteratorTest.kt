1d9d9a761db95a582a2b1cdf70fa428a164793c5
Kai Reinhard <K.Reinhard@micromata.de>
2020-01-01 01:06:56 +0100

    Testcase: ExcelSheet: test of customized columns for row emptiness check.

--- merlin-core/src/test/kotlin/de/micromata/merlin/excel/ExcelSheetRowIteratorTest.kt
+++ merlin-core/src/test/kotlin/de/micromata/merlin/excel/ExcelSheetRowIteratorTest.kt
@@ -4,20 +4,18 @@ import de.micromata.merlin.Definitions
 import org.apache.poi.ss.usermodel.Row
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 import java.io.File
+import java.time.Month
 
 internal class ExcelSheetRowIteratorTest {
-    private val log = LoggerFactory.getLogger(ExcelSheetRowIteratorTest::class.java)
     @Test
-    fun configReaderValidationTest() {
+    fun datarowIteratorTest() {
         val excelWorkbook = ExcelWorkbook(File(Definitions.EXAMPLES_EXCEL_TEST_DIR, "Workbook-Test.xlsx"))
         val excelSheet = excelWorkbook.getSheet("empty rows")!!
         excelSheet.registerColumn("E-Mail")
         excelSheet.registerColumn("Name")
         excelSheet.registerColumn("Age")
         excelSheet.registerColumn("Date")
-        val email2ColDef = excelSheet.registerColumn("E-Mail")
         val it: Iterator<Row> = excelSheet.dataRowIterator
         var row = it.next()
         Assertions.assertEquals("k.reinhard@acme.com", excelSheet.getCellString(row, "E-Mail"))
@@ -26,6 +24,33 @@ internal class ExcelSheetRowIteratorTest {
         Assertions.assertEquals("b.muster@acme.com", excelSheet.getCellString(row, "E-Mail"))
         row = it.next()
         Assertions.assertEquals("test", PoiHelper.getValueAsString(excelSheet.getCell(row.rowNum, 1)))
+        row = it.next()
+        val date = ExcelColumnDateValidator().getLocalDateTime(excelSheet.getCell(row, "Date"))!!
+        Assertions.assertEquals(1, date.dayOfMonth)
+        Assertions.assertEquals(Month.JANUARY, date.month)
+        Assertions.assertEquals(2020, date.year)
+        row = it.next()
+        Assertions.assertEquals("Test", excelSheet.getCellString(row, "E-Mail"))
+        Assertions.assertFalse(it.hasNext())
+    }
+
+    @Test
+    fun datarowIteratorColumnsTest() {
+        val excelWorkbook = ExcelWorkbook(File(Definitions.EXAMPLES_EXCEL_TEST_DIR, "Workbook-Test.xlsx"))
+        val excelSheet = excelWorkbook.getSheet("empty rows")!!
+        excelSheet.registerColumn("E-Mail")
+        excelSheet.registerColumn("Name")
+        excelSheet.registerColumn("Age")
+        excelSheet.registerColumn("Date")
+        excelSheet.setColumnsForRowEmptyCheck("E-Mail", "Name", "Age")
+        val it: Iterator<Row> = excelSheet.dataRowIterator
+        var row = it.next()
+        Assertions.assertEquals("k.reinhard@acme.com", excelSheet.getCellString(row, "E-Mail"))
+        Assertions.assertTrue(it.hasNext())
+        row = it.next()
+        Assertions.assertEquals("b.muster@acme.com", excelSheet.getCellString(row, "E-Mail"))
+        row = it.next()
+        Assertions.assertEquals("Test", excelSheet.getCellString(row, "E-Mail"))
         Assertions.assertFalse(it.hasNext())
     }
 }
